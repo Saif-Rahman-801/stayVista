f6bae0462795e358bfd71627870a0039e1f60275
Saif-Rahman-801 <saifurs102@gmail.com>
2024-01-25 14:36:04 +0600

    using the query string to change query

--- client/src/components/Rooms/Categories/Categorybox.jsx
+++ client/src/components/Rooms/Categories/Categorybox.jsx
@@ -1,8 +1,33 @@
 /* eslint-disable react/prop-types */
+import { useNavigate, useSearchParams } from "react-router-dom";
+import qs from "query-string";
 
 const Categorybox = ({ label, icon: Icon }) => {
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+      const updatedQuery = {
+        ...currentQuery,
+        category: label,
+      };
+
+      const url = qs.stringifyUrl({
+        url: "/",
+        query: updatedQuery,
+      });
+      navigate(url);
+    }
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center p-3 gap-2 border-b-2 cursor-pointer hover:text-neutral-800 transition">
+    <div
+      onClick={handleClick}
+      className="flex flex-col justify-center items-center p-3 gap-2 border-b-2 cursor-pointer hover:text-neutral-800 transition"
+    >
       <Icon size={26} />
       <div className="text-sm font-medium">{label} </div>
     </div>
