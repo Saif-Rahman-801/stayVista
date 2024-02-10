f46cd6499e37a23e405b2b37d5ff127115613ed5
Saif-Rahman-801 <saifurs102@gmail.com>
2024-02-10 18:57:37 +0600

    details page created and the details fetched

--- client/src/components/Rooms/RoomsCard.jsx
+++ client/src/components/Rooms/RoomsCard.jsx
@@ -1,58 +1,53 @@
 /* eslint-disable react/prop-types */
 
-const RoomsCard = ({room}) => {
-    return (
-        <div className='col-span-1 cursor-pointer group'>
-      <div className='flex flex-col gap-2 w-full'>
-        <div
-          className='
+import { Link } from "react-router-dom";
+const RoomsCard = ({ room }) => {
+  return (
+    <Link to={`/rooms/${room?._id}`}>
+      <div className="col-span-1 cursor-pointer group">
+        <div className="flex flex-col gap-2 w-full">
+          <div
+            className="
             aspect-square 
             w-full 
             relative 
             overflow-hidden 
             rounded-xl
-          '
-        >
-          <img
-            className='
+          "
+          >
+            <img
+              className="
               object-cover 
               h-full 
               w-full 
               group-hover:scale-110 
               transition
-            '
-            src={room?.image}
-            alt='Room'
-          />
-          <div
-            className='
+            "
+              src={room?.image}
+              alt="Room"
+            />
+            <div
+              className="
             absolute
             top-3
             right-3
-          '
-          >
-            {/* <HeartButton /> */}
+          "
+            >
+              {/* <HeartButton /> */}
+            </div>
           </div>
-        </div>
-        <div className='font-semibold text-lg'>
-            {
-                room?.location
-            }
-        </div>
-        <div className='font-light text-neutral-500'>
-          5 nights . June 19 - 26
-        </div>
-        <div className='flex flex-row items-center gap-1'>
-          <div className='font-semibold'>$
-            {
-                room?.price
-            } 
+          <div className="font-semibold text-lg">{room?.location}</div>
+          <div className="font-light text-neutral-500">
+            5 nights . June 19 - 26
           </div>
-          <div className='font-light'>night</div>
+          <div className="flex flex-row items-center gap-1">
+            <div className="font-semibold">${room?.price}</div>
+            <div className="font-light">night</div>
+          </div>
         </div>
       </div>
-    </div>
-    );
+    </Link>
+  );
 };
 
 export default RoomsCard;
