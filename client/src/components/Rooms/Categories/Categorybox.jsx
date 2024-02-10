fb1465080948e7690334f820ff26c3970243095a
Saif-Rahman-801 <saifurs102@gmail.com>
2024-02-10 10:02:04 +0600

    explained the search params

--- client/src/components/Rooms/Categories/Categorybox.jsx
+++ client/src/components/Rooms/Categories/Categorybox.jsx
@@ -3,22 +3,45 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import qs from "query-string";
 
 const Categorybox = ({ label, icon: Icon }) => {
+  // eslint-disable-next-line no-unused-vars
   const [params, setParams] = useSearchParams();
   const navigate = useNavigate();
 
   const handleClick = () => {
+    // Summary
+    /* 
+Goal -> on clicking on the div we want to navigate it by a query string url and we also want to access the value of query string in any components
+
+Steps ->
+1. To get the search params use react-router hook useSearchParams(); it returns URLSearchParams object 
+
+2. check if params object exists; if it exists then get the existing query by converting it to String and parse it to object using query string and hold it in a object
+
+3. Now create a updatedQuery object, copy the existing query object and update the category by the label on a click event. update it because there can be other query parameters that you don't want to change
+
+4. Now create a Url from updatedQuery by using qs.stringifyUrl() from query string and navigate to the url
+
+5. Now you can access the query parameter in any component by using params.get(query key)
+
+
+*/
+
     let currentQuery = {};
     if (params) {
       currentQuery = qs.parse(params.toString());
+      // console.log(currentQuery);
+
       const updatedQuery = {
         ...currentQuery,
         category: label,
       };
+      // console.log(updatedQuery);
 
       const url = qs.stringifyUrl({
         url: "/",
         query: updatedQuery,
       });
+      // console.log(url);
       navigate(url);
     }
   };
